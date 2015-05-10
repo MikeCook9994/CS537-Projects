aac9023401ebd93d96ffa37fbc43ce5f8a65acd7
MikeCook9994 <mcook4728@gmail.com>
2015-05-10 12:03:54 -0500

    pushing hella changes

--- projects/5/fsck/fsck.c
+++ projects/5/fsck/fsck.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <string.h>
 #include "fs.h"
 
 int fsd;
@@ -25,24 +26,32 @@ void printinode(struct dinode * dinode) {
 	printf("minor:\t\t%hd\n", dinode->minor); // Minor device number
 	printf("nlink:\t\t%hd\n", dinode->nlink); // Number of links to the inode on the system
 	printf("size:\t\t%u\n", dinode->size); // Size of the block
-	printf("addr[0]:\t0x%x\n", dinode->addrs[0]); // Data block pointer addresses
-	printf("addr[1]:\t0x%x\n", dinode->addrs[1]);
-	printf("addr[2]:\t0x%x\n", dinode->addrs[2]);
-	printf("addr[3]:\t0x%x\n", dinode->addrs[3]);
-	printf("addr[4]:\t0x%x\n", dinode->addrs[4]);
-	printf("addr[5]:\t0x%x\n", dinode->addrs[5]);
-	printf("addr[6]:\t0x%x\n", dinode->addrs[6]);
-	printf("addr[7]:\t0x%x\n", dinode->addrs[7]);
-	printf("addr[8]:\t0x%x\n", dinode->addrs[8]);
-	printf("addr[9]:\t0x%x\n", dinode->addrs[9]);
-	printf("addr[10]:\t0x%x\n", dinode->addrs[10]);
-	printf("addr[11]:\t0x%x\n", dinode->addrs[11]);
-	printf("addr[12]:\t0x%x\n", dinode->addrs[12]);
-	printf("addr[13]:\t0x%x\n\n", dinode->addrs[13]); // Indirect pointer address */
+	
+	int i;
+	for(i = 0; i < 14; i++) {
+		printf("addr[%d]:\t0x%x\n", i, dinode->addrs[i]);
+	}
+	printf("\n");	
+}
+
+void printSuperBlock(void) {
+	printf("SUPER BLOCK CONTENTS\n");
+	printf("size:\t\t%d\n", sb->size);
+	printf("nblocks:\t%d\n", sb->nblocks);
+	printf("ninodes:\t%d\n\n", sb->ninodes);
 }
 
 /* sniffs the super block for inconsistent contents. */
 int sniffsb(uint * size, uint nblocks, uint ninodes) {
+
+	/* repairs the super block by correcting the size value if the size times block size 
+ 	 * does not equal the file size */
+	if(*size * BSIZE != (int) stats->st_size) {
+		printf("Repairing Superblock size value\n\n");
+		*size = stats->st_size / BSIZE;
+		seek(BSIZE);
+		write(fsd, size, 4);
+	}
 	/* verifies the size is greater than zero */
 	if(*size <= 0)
 		return -1;
@@ -52,33 +61,51 @@ int sniffsb(uint * size, uint nblocks, uint ninodes) {
 	/* verifies the number of nodes is a multiple of the number of inodes per block */
 	if(ninodes % IPB != 0)
 		return -1;
-	/* if the number of inodes exceeds the size returns -1 */
-	if(ninodes > *size)
+	/* verifies that the total number of blocks is greater than or equal to the 
+	   the number of data blocks plus the number of inode blocks */
+	if(nblocks + (ninodes / IPB) > *size)
 		return -1;
-	 /* repairs the super block by correcting the size value if the size times block size 
- 	 * does not equal the file size */
-	if(*size * BSIZE != (int) stats->st_size) {
-		printf("Repairing Superblock size value\n");
-		*size = stats->st_size / BSIZE;
-	}
 	/* the block doesn't smell */
 	return 0;
+}
+
+void clearinode(struct dinode * inode) {
+	printf("Cleared Inode\n");
+	memset(inode, 0, sizeof(struct dinode));
+}
+
+void checkinode(struct dinode * inode) {
+	/* checks the inode reference count */
+	if(inode->type == 0) {
+		return;
+		/* free the data bitmap bit */		
+	}
+	if(inode->nlink == 0 || inode->nlink >= MAXFILE) {
+		clearinode(inode);	
+	}
+	/* checks the inode type */
+	if(inode->type > 3 || inode->type < 0) {
+		clearinode(inode);
+	}
+	if(inode->size < 0 || inode->size > (sb->size * BSIZE)) {
+		clearinode(inode);
+	}
 }
 
 int main(int charc, char * argv[]) {
 
 	/* validates that an input file name is provided */
-	if(charc != 2) {
+	if(charc < 2) {
 		printf("No valid system image file name provided.\n");
 		return -1;
 	}
 
 	/* allocates space for the superblock and file stats structs */
 	sb = malloc(sizeof(struct superblock));
-	stats = malloc(sizeof(struct stat));	
+	assert(sb != NULL);	
 
-	assert(sb != NULL);
-	assert(fstat != NULL);
+	stats = malloc(sizeof(struct stat));	
+	assert(fstat != NULL);	
 
 	/* attempts to open the provided fs image */
 	fsd = open(argv[1], O_RDWR);
@@ -87,6 +114,7 @@ int main(int charc, char * argv[]) {
 	/* stats the file */
 	fstat(fsd, stats);
 
+	/* prints the welcome messgae */
 	printf("*~*~*~*~*~*~*~*~*~*~*~*\n");
 	printf("*   WELCOME TO FSCK   *\n");
 	printf("* YOUR SHIT IS FUCKED *\n");
@@ -97,28 +125,23 @@ int main(int charc, char * argv[]) {
 
 	/* populates the superblock struct and prints its contents */
 	assert(read(fsd, sb, sizeof(struct superblock)) != -1);
-	printf("CHECKING SUPER BLOCK CONTENTS\n");
-	printf("size:\t\t%d\n", sb->size);
-	printf("nblocks:\t%d\n", sb->nblocks);
-	printf("ninodes:\t%d\n\n", sb->ninodes);
-
-	/* allocates space for enumerating the inodes and the inode bitmap*/	
-	inodeList = malloc(sizeof(struct dinode) * sb->ninodes);
-	inodebitmap = malloc(sizeof(int) * (sb->ninodes / IPB));
-	assert(inodeList != NULL);
+	printSuperBlock();
 
 	/* a pointer to size value stored in the superblock that allows the super block to be repaired */
 	uint * sbsize;
 	sbsize = &sb->size;
 
 	/* callst the function that sniffs the super block */
-	if(sniffsb(sbsize, sb->nblocks, sb->ninodes) < 0) {
-		printf("Inrepairable Super Block State Found. The FS Image cannot be repaired. Exiting.\n");
+	if(sniffsb(sbsize, sb->nblocks, sb->ninodes) != 0) {
+		printf("Error\n");
 		exit(0);
 	}
+	printSuperBlock();
 
-	/* nagivates to the start of the inodes */
-	seek(BSIZE * IBLOCK(0));
+	/* allocates space for enumerating the inodes and the inode bitmap*/	
+	inodeList = malloc(sizeof(struct dinode) * sb->ninodes);	
+	assert(inodeList != NULL);
+
 	/* skips the garbage inode and sets you to the root inode */
 	seek(BSIZE * IBLOCK(1) + sizeof(struct dinode));
 
@@ -126,15 +149,22 @@ int main(int charc, char * argv[]) {
 	int i;
 	for(i = 0; i < sb->ninodes; i++) {
 		assert(read(fsd, inodeList + i, sizeof(struct dinode)) != -1);
-	//	printinode(inodeList + i);
+		printf("%d\n", i+1);
+		printinode(inodeList + i);
+		checkinode(inodeList + i);
+		printinode(inodeList + i);
 	}
+	write(fsd, inodeList, (sizeof(inodeList)));
+
+	inodebitmap = malloc(sizeof(int) * (sb->ninodes / IPB));
+	assert(inodebitmap != NULL);
 
 	/* seeks to the beginning of the inode bitmap */
 	seek(BSIZE * (1 /* garbage block */ + 1 /* super block */ + (sb->ninodes / IPB) /* number of blocks occuppied by inodes */ + 1 /* bitmap */));
 
 	/* reads in the bit map */
 	for(i = 0; i < sb->ninodes; i++) {
-		assert(read(fsd, inodebitmap + i, sizeof(int)) != -1);
+		assert(read(fsd, inodebitmap + i, sizeof(int)) != -1);	
 	}
 
 	printf("FSCK complete. Exiting.\n");
--- projects/5/lfsreader/lfsreader.c
+++ projects/5/lfsreader/lfsreader.c
@@ -1,11 +1,21 @@
+#define BLKSIZE 4096
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <assert.h>
+#include "structdefs.h"
 
 int fsd;
+checkpoint * ckpt;
+inodeMap * imap[INODEPIECES];
+
+int seek(int location) {
+	return (int) lseek(fsd, location, SEEK_SET);
+}
 
 void error(void) {
 	printf("Error!\n");
@@ -25,5 +35,23 @@ int main(int charc, char *argv[]) {
 	if(fsd < 0)
 		error();
 
+	/* allocates space for the checkpoint region struct */
+	ckpt = malloc(sizeof(checkpoint));
+
+	/* reads in the checkpoint region */
+	assert(read(fsd, ckpt, sizeof(checkpoint)) != -1);	
+
+
+	/* locates all the imap regions and sticks them in an array for latter access */
+	int i;
+	for(i = 0; i < INODEPIECES; i++) {
+			imap[i] = (ckpt->iMapPtr[i] + (void *)ckpt); 
+			printf("imap[%d] address: %p\n", i, imap[i]);
+			int j;
+			for(j = 0; j < 16; j++) {
+			printf("imap[%d]->inodePtr[%d]: %d\n", i, j, imap[i]->inodePtr[j]);
+		} 
+	}
+	
 	return 0;
 }
