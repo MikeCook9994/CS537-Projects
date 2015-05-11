2a7b2d32e2a04c1cf299a45c4d86efa6d6ce48ef
Michael Cook <mjcook3@wisc.edu>
2015-05-11 01:17:33 -0500

    almost finished lfsreader

--- projects/5/lfsreader/lfsreader.c
+++ projects/5/lfsreader/lfsreader.c
@@ -26,6 +26,39 @@ void peruse(void * buf, int count) {
 
 void error(void) {
 	printf("Error!\n");
+	exit(0);
+}
+
+void printckpt(checkpoint * ckpt) {
+	printf("ckpt->size = %d\n", ckpt->size);
+	int i;
+	for(i = 0; i < INODEPIECES; i++) {
+		printf("ckpt->iMapPtr[%d] = %d\n", i, ckpt->iMapPtr[i]);
+	}
+}
+
+void printdir(dirEnt * dir) {
+	printf("dir->name = %s\n", dir->name);
+	printf("dir->inum = %d\n", dir->inum);
+}
+
+void printinode(inode * inode) {
+	printf("inode->size = %d\n", inode->size);
+	if(inode->type == 0)
+		printf("inode->type = MFS_DIRECTORY\n");
+	else
+		printf("inode->type = MFS_REGULAR_FILE\n");
+	int i;
+	for(i = 0; i < 14; i++) {
+		printf("inode->ptr[%d] = %d\n", i, inode->ptr[i]);
+	}
+}
+
+void printinodeMap(inodeMap * inodemap) {
+	int i;
+	for(i = 0; i < 16; i++) {
+		printf("inodemap->inodePtr[%d] = %d\n", i, inodemap->inodePtr[i]);
+	}
 }
 
 struct node * tokenizeString(char * string) {
@@ -56,6 +89,75 @@ struct node * tokenizeString(char * string) {
 	}
 
 	return head;
+}
+
+int findinode(inode * inode, char * string) {
+
+	int i;
+	for(i = 0; i < 14; i++) {
+		if(inode->ptr[i] == -1)
+			break;
+		
+		seek(inode->ptr[i]);
+		
+		dirEnt * tmp;
+		tmp = malloc(sizeof(dirEnt));
+
+		while(1) {
+			peruse(tmp, sizeof(dirEnt));
+			if(strcmp(string, tmp->name) == 0)
+				return tmp->inum;
+			if(tmp->inum == -1)
+				break;
+			
+		}	
+	}
+
+	return -1;
+}
+
+void ls(inode * inode) {
+	int i;
+	for(i = 0; i < 14; i++) {
+		if(inode->ptr[i] == -1)
+			break;
+		
+		seek(inode->ptr[i]);
+		
+		dirEnt * tmp;
+		tmp = malloc(sizeof(dirEnt));
+
+		while(1) {
+			peruse(tmp, sizeof(dirEnt));
+			if(tmp->inum == -1)
+				break;
+			printf("%s\n", tmp->name);
+		}	
+	}
+}
+
+void cat(inode * inode) {
+	
+	int size;
+	size = inode->size;
+
+	char * buf;
+	buf = malloc(size);
+
+	seek(inode->ptr[0]);
+	peruse(buf, size);
+
+	int i;
+	for(i = 0; i < 16; i++) {
+		if(size >= BLKSIZE) {
+			write(1, buf, BLKSIZE);
+			size -= BLKSIZE;
+		}
+		else {
+			write(1, buf, size);
+			break;
+		}
+	}
 }
 
 int main(int charc, char *argv[]) {
@@ -95,7 +197,49 @@ int main(int charc, char *argv[]) {
 	seek(rootinodeMap->inodePtr[0]);
 	peruse(rootiNode, sizeof(inode));
 
+	Node * curr;
+	curr = head;
+
+	int currinodenum;
+	currinodenum = 0;
 	
+	inode * currinode;
+	currinode = rootiNode;
+
+	/* navaigates to the highest level directory or file based on the path name */
+	while(curr != NULL) {
+		currinodenum = findinode(currinode, curr->string);
+		if(currinodenum == -1)
+			error();
+		curr = curr->next;
+
+		inodeMap * tmp;
+		tmp = malloc(sizeof(inodeMap));
+		
+		seek(ckpt->iMapPtr[currinodenum / 16]);
+		peruse(tmp, sizeof(inodeMap));
+
+		seek(tmp->inodePtr[currinodenum % 16]);
+		peruse(currinode, sizeof(inode));
+	}
+
+	inode * finalinode = currinode;
+
+	if(strcmp(argv[1], "ls\0") == 0) {
+		if(finalinode->type == 1)
+			error();
+		else
+			ls(finalinode);
+	}
+	else if (strcmp(argv[1], "cat\0") == 0) {
+		if(finalinode->type == 0)
+			error();
+		else
+			cat(finalinode);
+	}
+	else {
+		error();
+	}
 
 	return 0;
 }
