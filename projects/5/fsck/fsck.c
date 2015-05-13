982bd5661628b9d556d833c697312c1cbb5882d9
MikeCook9994 <mcook4728@gmail.com>
2015-05-13 16:10:14 -0500

    no

--- projects/5/fsck/fsck.c
+++ projects/5/fsck/fsck.c
@@ -321,9 +321,6 @@ int main(int charc, char * argv[]) {
 			lostandfound(inodeList + i);
 	}
 
-	seek(IBLOCK(0));
-	write(fsd, inodeList, sizeof(struct dinode) * sb->ninodes);
-
 	close(fsd);	
 
 	return 0;	
